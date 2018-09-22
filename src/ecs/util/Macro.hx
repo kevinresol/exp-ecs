d38ec0061e80d81d8addbf685e1a1f2c6b7ce100
Kevin Leung <kevinresol@gmail.com>
2018-09-22 12:09:53 +0800

    Use BuildCache to cache Node

--- src/ecs/util/Macro.hx
+++ src/ecs/util/Macro.hx
@@ -2,141 +2,141 @@ package ecs.util;
 
 import haxe.macro.Expr;
 import haxe.macro.Context;
+import tink.macro.BuildCache;
+
 using tink.MacroApi;
 using StringTools;
 
 class Macro {
 	
 	public static function buildNode() {
-		var pos = Context.currentPos();
-		var params = switch Context.getLocalType() {
-			case TInst(_, params): params;
+		var types = switch Context.getLocalType() {
+			case TInst(_, params):
+				params.sort(function(t1, t2) return Reflect.compare(t1.getID(), t2.getID()));
+				params;
 			default: throw 'assert';
 		}
 		
-		var fullnames = [];
-		var names = []; 
-		var complexTypes = [];
-		for(param in params) {
-			var type = param.reduce();
-			switch type {
-				case TInst(_.get() => cls, _) if(isComponent(type)):
-					fullnames.push(cls.pack.concat([cls.name]).join('_'));
-					names.push(cls.name.substr(0, 1).toLowerCase() + cls.name.substr(1));
-					complexTypes.push(param.toComplex());
-				default: pos.makeFailure('Expected a class that extends Component, but got ${type.getID()}').sure();
+		return BuildCache.getTypeN('ecs.node.Node', types, function(ctx:BuildContextN) {
+			var pos = ctx.pos;
+			var name = ctx.name;
+			
+			var fullnames = [];
+			var names = []; 
+			var complexTypes = [];
+			for(type in ctx.types) {
+				switch type.reduce() {
+					case TInst(_.get() => cls, _) if(isComponent(type)):
+						names.push(cls.name.substr(0, 1).toLowerCase() + cls.name.substr(1));
+						complexTypes.push(type.toComplex());
+					default: pos.makeFailure('Expected a class that extends Component, but got ${type.getID()}').sure();
+				}
 			}
-		}
-		fullnames.sort(Reflect.compare);
-		var name = 'Node_' + Context.signature(fullnames.join('_'));
-		try return Context.getType('ecs.node.$name') catch(e:Dynamic) {}
-		
-		var tp = 'ecs.node.$name'.asTypePath();
-		var arr = complexTypes.map(function(ct) return macro $p{ct.toString().split('.')});
-		var ctorArgs = [macro entity];
-		for(ct in complexTypes) ctorArgs.push(macro entity.get($p{ct.toString().split('.')}));
 			
-		var def = macro class $name implements ecs.node.NodeBase {
-			public var entity(default, null):ecs.entity.Entity;
-			
-			public static var componentTypes:Array<ecs.component.ComponentType> = $a{arr};
-			public static function createNodeList(engine:ecs.Engine) {
-				var nodes = new ecs.node.NodeList();
-				var listeners = new Map();
+			var tp = 'ecs.node.$name'.asTypePath();
+			var arr = complexTypes.map(function(ct) return macro $p{ct.toString().split('.')});
+			var ctorArgs = [macro entity];
+			for(ct in complexTypes) ctorArgs.push(macro entity.get($p{ct.toString().split('.')}));
 				
-				inline function addEntityIfMatch(entity:ecs.entity.Entity) 
-					if(entity.hasAll(componentTypes))
-						nodes.add(new $tp($a{ctorArgs}));
-						
-				inline function removeEntityIfNoLongerMatch(entity:ecs.entity.Entity) 
-					if(!entity.hasAll(componentTypes))
-						nodes.removeEntity(entity);
+			var def = macro class $name implements ecs.node.NodeBase {
+				public var entity(default, null):ecs.entity.Entity;
 				
-				inline function track(entity:ecs.entity.Entity) {
-					if(listeners.exists(entity)) return; // already tracking
-					listeners.set(entity, [
-						entity.componentAdded.handle(function() addEntityIfMatch(entity)),
-						entity.componentRemoved.handle(function() removeEntityIfNoLongerMatch(entity)),
-					]);
-				}
-				
-				inline function untrack(entity:ecs.entity.Entity) {
-					if(!listeners.exists(entity)) return; // not tracking
-					var l = listeners.get(entity);
-					while(l.length > 0) l.pop().dissolve();
-					listeners.remove(entity);
-				}
-				
-				for(entity in engine.entities) {
-					addEntityIfMatch(entity);
-					track(entity);
-				}
+				public static var componentTypes:Array<ecs.component.ComponentType> = $a{arr};
+				public static function createNodeList(engine:ecs.Engine) {
+					var nodes = new ecs.node.NodeList();
+					var listeners = new Map();
 					
-				// TODO: if we destroy a node list, we need to dissolve the handlers
-				engine.entityAdded.handle(function(e) {
-					addEntityIfMatch(e);
-					track(e);
-				});
-				engine.entityRemoved.handle(function(e) {
-					nodes.removeEntity(e);
-					untrack(e);
-				});
+					inline function addEntityIfMatch(entity:ecs.entity.Entity) 
+						if(entity.hasAll(componentTypes))
+							nodes.add(new $tp($a{ctorArgs}));
+							
+					inline function removeEntityIfNoLongerMatch(entity:ecs.entity.Entity) 
+						if(!entity.hasAll(componentTypes))
+							nodes.removeEntity(entity);
+					
+					inline function track(entity:ecs.entity.Entity) {
+						if(listeners.exists(entity)) return; // already tracking
+						listeners.set(entity, [
+							entity.componentAdded.handle(function() addEntityIfMatch(entity)),
+							entity.componentRemoved.handle(function() removeEntityIfNoLongerMatch(entity)),
+						]);
+					}
+					
+					inline function untrack(entity:ecs.entity.Entity) {
+						if(!listeners.exists(entity)) return; // not tracking
+						var l = listeners.get(entity);
+						while(l.length > 0) l.pop().dissolve();
+						listeners.remove(entity);
+					}
+					
+					for(entity in engine.entities) {
+						addEntityIfMatch(entity);
+						track(entity);
+					}
+						
+					// TODO: if we destroy a node list, we need to dissolve the handlers
+					engine.entityAdded.handle(function(e) {
+						addEntityIfMatch(e);
+						track(e);
+					});
+					engine.entityRemoved.handle(function(e) {
+						nodes.removeEntity(e);
+						untrack(e);
+					});
+					
+					return nodes;
+				}
 				
-				return nodes;
 			}
 			
-		}
-		
-		var ctorArgs = [{
-			name: 'entity',
-			type: macro:ecs.entity.Entity,
-			opt: false,
-			meta: null,
-			value: null,
-		}]; 
-		for(i in 0...names.length) {
-			var name = names[i];
-			var ct = complexTypes[i];
-			
-			// Prepare the constructor args for the node class
-			ctorArgs.push({
-				name: name,
-				type: ct,
+			var ctorArgs = [{
+				name: 'entity',
+				type: macro:ecs.entity.Entity,
 				opt: false,
 				meta: null,
 				value: null,
-			});
+			}]; 
+			for(i in 0...names.length) {
+				var name = names[i];
+				var ct = complexTypes[i];
+				
+				// Prepare the constructor args for the node class
+				ctorArgs.push({
+					name: name,
+					type: ct,
+					opt: false,
+					meta: null,
+					value: null,
+				});
+				
+				// Create instance field for each component, named in the component's class name camel-cased
+				def.fields.push({
+					name: name,
+					kind: FVar(ct),
+					pos: pos,
+					access: [APublic],
+					meta: null,
+				});
+			}
 			
-			// Create instance field for each component, named in the component's class name camel-cased
+			// constructor
+			var exprs = names.map(function(name) return macro this.$name = $i{name});
+			exprs.push(macro this.entity = entity);
 			def.fields.push({
-				name: name,
-				kind: FVar(ct),
+				name: 'new',
+				kind: FFun({
+					args: ctorArgs,
+					expr: macro $b{exprs},
+					ret: null,
+				}),
 				pos: pos,
 				access: [APublic],
-				meta: null,
+				meta: null
 			});
-		}
-		
-		// constructor
-		var exprs = names.map(function(name) return macro this.$name = $i{name});
-		exprs.push(macro this.entity = entity);
-		def.fields.push({
-			name: 'new',
-			kind: FFun({
-				args: ctorArgs,
-				expr: macro $b{exprs},
-				ret: null,
-			}),
-			pos: pos,
-			access: [APublic],
-			meta: null
+			def.pack = ['ecs', 'node'];
+			
+			return def;
 		});
-		def.pack = ['ecs', 'node'];
-		
-		Context.defineType(def);
-		return Context.getType('ecs.node.$name');
-		
-		
 	}
 	
 	public static function buildNodeListSystem():Array<Field> {
