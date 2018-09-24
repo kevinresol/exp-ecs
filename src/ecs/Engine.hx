90cdab64996d1fccda4979e1222556031a84609b
Kevin Leung <kevinresol@gmail.com>
2018-09-24 11:35:30 +0800

    Add ReadOnlyArray

--- src/ecs/Engine.hx
+++ src/ecs/Engine.hx
@@ -9,48 +9,50 @@ using tink.CoreApi;
 
 class Engine {
 	
-	public var entities(default, null):Array<Entity>;
+	public var entities(default, null):ReadOnlyArray<Entity>;
 	public var entityAdded(default, null):Signal<Entity>;
 	public var entityRemoved(default, null):Signal<Entity>;
+	public var systems:ReadOnlyArray<SystemBase>;
 	
-	var systems:Array<SystemBase>;
+	var _entities:Array<Entity>;
+	var _systems:Array<SystemBase>;
 	var nodeLists:Map<NodeType, NodeList<Dynamic>>;
 	
 	var entityAddedTrigger:SignalTrigger<Entity>;
 	var entityRemovedTrigger:SignalTrigger<Entity>;
 	
 	public function new() {
-		entities = [];
-		systems = [];
+		entities = _entities = [];
+		systems = _systems = [];
 		nodeLists = new Map();
 		entityAdded = entityAddedTrigger = Signal.trigger();
 		entityRemoved = entityRemovedTrigger = Signal.trigger();
 	}
 	
 	public function update(dt:Float) {
-		for(system in systems)
+		for(system in _systems)
 			system.update(dt);
 	}
 	
 	public function addEntity(entity:Entity) {
 		removeEntity(entity); // re-add to the end of the list
-		entities.push(entity);
+		_entities.push(entity);
 		entityAddedTrigger.trigger(entity);
 	}
 	
 	public function removeEntity(entity:Entity) {
-		if(entities.remove(entity))
+		if(_entities.remove(entity))
 			entityRemovedTrigger.trigger(entity);
 	}
 	
 	public function addSystem(system:System) {
 		removeSystem(system); // re-add to the end of the list
-		systems.push(system);
+		_systems.push(system);
 		system.onAdded(this);
 	}
 	
 	public function removeSystem(system:System) {
-		if(systems.remove(system))
+		if(_systems.remove(system))
 			system.onRemoved(this);
 	}
 	
