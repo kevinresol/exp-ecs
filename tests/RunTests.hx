de9374c254ff3984fbf64297c68dcd6c2ce242e6
Kevin Leung <kevinresol@gmail.com>
2018-09-24 11:36:19 +0800

    Add a bit of benchmarking

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -20,6 +20,7 @@ class RunTests {
 			new EngineTest(),
 			new StateMachineTest(),
 			new NodeListTest(),
+			new NodeListBenchmark(),
 		])).handle(Runner.exit);
 	}
 }
@@ -142,6 +143,36 @@ class NodeListTest {
 			asserts.done();
 		});
 		return asserts;
+	}
+}
+
+class NodeListBenchmark implements Benchmark {
+	
+	var list:NodeList<MovementNode>;
+	
+	public function new() {}
+	
+	@:before
+	public function before() {
+		var engine = new Engine();
+		for(i in 0...1000) {
+			var entity = new Entity();
+			entity.add(new Velocity(0, 0));
+			entity.add(new Position(0, 0));
+			engine.addEntity(entity);
+		}
+		list = engine.getNodeList(MovementNode, MovementNode.createNodeList);
+		return Noise;
+	}
+	
+	@:benchmark(10000)
+	public function iterate() {
+		for(node in list) {
+			var pos = node.position;
+			var vel = node.velocity;
+			pos.x += vel.x;
+			pos.y += vel.y;
+		}
 	}
 }
 
