b486f17105d0c41656583f115d2bb0fd55cfb6e0
Kevin Leung <kevinresol@gmail.com>
2018-11-06 20:41:32 +0800

    GameOver

--- sample/asteroid/src/GameState.hx
+++ sample/asteroid/src/GameState.hx
@@ -4,6 +4,7 @@ class GameState {
 	public var lives = 3;
 	public var level = 0;
 	public var points = 0;
+	public var over = false;
 	
 	public function new() {}
 }
--- sample/asteroid/src/system/GameSystem.hx
+++ sample/asteroid/src/system/GameSystem.hx
@@ -20,35 +20,38 @@ class GameSystem<Event:EnumValue> extends System<Event> {
 	}
 	
 	override function update(dt:Float) {
-		if(spaceships.empty) {
-			if(game.lives > 0) {
-				var newPos = new Point(config.width * 0.5, config.height * 0.5);
-				var clear = true;
-				for(asteroid in asteroids) {
-					if(Point.distance(asteroid.position.position, newPos) <= asteroid.collision.radius + 50) {
-						clear = false;
-						break;
+		if(!game.over) {
+			if(spaceships.empty) {
+				if(game.lives > 0) {
+					var newPos = new Point(config.width * 0.5, config.height * 0.5);
+					var clear = true;
+					for(asteroid in asteroids) {
+						if(Point.distance(asteroid.position.position, newPos) <= asteroid.collision.radius + 50) {
+							clear = false;
+							break;
+						}
 					}
+					if(clear) {
+						engine.entities.add(new entity.Spaceship(newPos.x, newPos.y));
+						game.lives--;
+					}
+				} else {
+					// game over
+					trace('Game Over');
+					game.over = true;
 				}
-				if(clear) {
-					engine.entities.add(new entity.Spaceship(newPos.x, newPos.y));
-					game.lives--;
-				}
-			} else {
-				// game over
 			}
-		}
-			
-		if(asteroids.empty && bullets.empty && !spaceships.empty) {
-			trace('empty');
-			// next level
-			var spaceship = spaceships.head;
-			game.level++;
-			for(i in 0... 2 + game.level) {
-				var position = null;
-				do position = new Point(Math.random() * config.width, Math.random() * config.height)
-				while(Point.distance(position, spaceship.position.position) < 80);
-				engine.entities.add(new entity.Asteroid(30, position.x, position.y));
+				
+			if(asteroids.empty && bullets.empty && !spaceships.empty) {
+				// next level
+				var spaceship = spaceships.head;
+				game.level++;
+				for(i in 0... 2 + game.level) {
+					var position = null;
+					do position = new Point(Math.random() * config.width, Math.random() * config.height)
+					while(Point.distance(position, spaceship.position.position) < 80);
+					engine.entities.add(new entity.Asteroid(30, position.x, position.y));
+				}
 			}
 		}
 	}
