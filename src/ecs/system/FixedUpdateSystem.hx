2316ffd435d4c365c9a6f0fd203e802af31caeb9
Kevin Leung <kevinresol@gmail.com>
2018-11-07 17:44:08 +0800

    A naive implementation of fixed-update system

---
+++ src/ecs/system/FixedUpdateSystem.hx
@@ -0,0 +1,22 @@
+package ecs.system;
+
+class FixedUpdateSystem<Event> extends System<Event> {
+	
+	var delta:Float;
+	var residue:Float = 0;
+	
+	public function new(delta = 0.01) {
+		super();
+		this.delta = delta;
+	}
+	
+	override function update(dt:Float) {
+		residue += dt;
+		while(residue > delta) {
+			fixedUpdate(delta);
+			residue -= delta;
+		}
+	}
+	
+	function fixedUpdate(dt:Float) {}
+}
