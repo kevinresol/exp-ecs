cb6c6d49bea1409217427337d133eb7ba4e4d317
Kevin Leung <kevinresol@gmail.com>
2019-02-02 13:32:53 +0800

    Document the event system

--- src/ecs/event/EventEmitter.hx
+++ src/ecs/event/EventEmitter.hx
@@ -3,44 +3,65 @@ package ecs.event;
 import ecs.util.*;
 using tink.CoreApi;
 
+@:allow(ecs)
 class EventEmitter<Event> {
 	var trigger:SignalTrigger<Event>;
 	var postSystemUpdate:Array<Event>;
 	var postEngineUpdate:Array<Event>;
 	
-	public function new() {
+	function new() {
 		trigger = Signal.trigger();
 		postSystemUpdate = [];
 		postEngineUpdate = [];
 	}
 	
+	/**
+	 * Register a callback that will be invoked for each event emitted
+	 * @param f
+	 */
 	public inline function handle(f) {
 		return trigger.asSignal().handle(f);
 	}
 	
+	/**
+	 * Create a filtered and transformed Signal.
+	 * @param f A filter+transform function. Return `None` to discard an event. Returning `Some(data)` will cause the resulting Signal to emit `data`.
+	 */
 	public inline function select(f) {
 		return trigger.asSignal().select(f);
 	}
 	
+	/**
+	 * Emit an event immediately
+	 * @param v 
+	 */
 	public inline function immediate(v:Event) {
 		trigger.trigger(v);
 	}
 	
+	/**
+	 * Emit an event after the current running System finishes its current `update`
+	 * @param v 
+	 */
 	public inline function afterSystemUpdate(v:Event) {
 		postSystemUpdate.push(v);
 	}
 	
+	/**
+	 * Emit an event after the Engine finishes its current `update`
+	 * @param v 
+	 */
 	public inline function afterEngineUpdate(v:Event) {
 		postEngineUpdate.push(v);
 	}
 	
-	public function flushSystem() {
+	function flushSystem() {
 		if(flush(postSystemUpdate))
 			postSystemUpdate = [];
 			// TODO: use this in haxe 4: postSystemUpdate.resize(0);
 	}
 	
-	public function flushUpdate() {
+	function flushUpdate() {
 		if(flush(postEngineUpdate))
 			postEngineUpdate = [];
 			// TODO: use this in haxe 4: postEngineUpdate.resize(0);
--- src/ecs/system/EventHandlerSystem.hx
+++ src/ecs/system/EventHandlerSystem.hx
@@ -3,13 +3,30 @@ package ecs.system;
 import ecs.event.*;
 using tink.CoreApi;
 
+/**
+ * A System that monitors the engine event bus.
+ */
 class EventHandlerSystem<Event, Data> extends System<Event> {
 	
 	var binding:CallbackLink;
 	
+	/**
+	 * This function will get invoked every time an event is fired in the engine event bus.
+	 * Use this function to filter and extract data from an event.
+	 * If we are not interested in a event, simply return `None`.
+	 * Otherwise if `Some(data)` is returned from this function, the `handle` function will be invoked accordingly.
+	 * @param event 
+	 * @return Option<Data>
+	 */
 	function select(event:Event):Option<Data>
 		return None;
 	
+	/**
+	 * This function will be used to handle event data selected by the `select` function.
+	 * You can produce the desired effect right in this function.
+	 * Or you can also store up the data and handle them later, e.g. in the `update` function
+	 * @param data 
+	 */
 	function handle(data:Data) {}
 	
 	override function onAdded(engine) {
