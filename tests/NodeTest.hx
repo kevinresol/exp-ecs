8db5ffd54b93f5da46a11f1b25cd05b4a0197f27
Kevin Leung <kevinresol@gmail.com>
2019-05-13 01:21:03 +0800

    Organize NodeTest a bit

--- tests/NodeTest.hx
+++ tests/NodeTest.hx
@@ -10,16 +10,14 @@ class NodeTest extends Base {
 		var entity = new Entity();
 		
 		var node1 = new Node<Velocity>(entity);
-		var node2 = new Node<Velocity>(entity);
+		var node2 = new Node<Velocity>(entity); // duplicated just to make sure they generate the same type
 		asserts.assert(getClassName(node1) == getClassName(node2));
 		
 		var node1 = new Node<Velocity, Position>(entity);
-		var node2 = new Node<Velocity, Position>(entity);
+		var node2 = new Node<Velocity, Position>(entity); // duplicated just to make sure they generate the same type
+		var node3 = new Node<Position, Velocity>(entity);
 		asserts.assert(getClassName(node1) == getClassName(node2));
-		
-		var node1 = new Node<Position, Velocity>(entity);
-		var node2 = new Node<Velocity, Position>(entity);
-		asserts.assert(getClassName(node1) == getClassName(node2));
+		asserts.assert(getClassName(node2) == getClassName(node3));
 		
 		return asserts.done();
 	}
