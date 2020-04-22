4185cbc004a02ce6599d53d83d80a1b202806ad1
Kevin Leung <kevinresol@gmail.com>
2020-04-22 10:09:41 +0800

    Add test

--- tests/NodeTest.hx
+++ tests/NodeTest.hx
@@ -21,4 +21,28 @@ class NodeTest extends Base {
 		
 		return asserts.done();
 	}
+	
+	public function componentChange() {
+		var entity = new Entity();
+		var vel1 = new Velocity(1, 1);
+		var vel2 = new Velocity(2, 2);
+		var node = new Node<Velocity>(entity);
+		
+		asserts.assert(node.velocity == null, 'Initial reference');
+		
+		// new component
+		entity.add(vel1);
+		asserts.assert(node.velocity == vel1, 'Add reference to component');
+		
+		// change component
+		entity.add(vel2);
+		asserts.assert(node.velocity == vel2, 'Change reference to component');
+		
+		// remove component
+		entity.remove(Velocity);
+		asserts.assert(node.velocity == null, 'Remove reference to component');
+		
+		
+		return asserts.done();
+	}
 }
